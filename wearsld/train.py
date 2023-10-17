17ed456d61e009daecb9072c65a793e8f0aab5fa
Felix Finkeldey <felix.finkeldey@tu-dortmund.de>
2023-10-17 10:47:08 +0200

    suppress sklearn warnings

--- wearsld/train.py
+++ wearsld/train.py
@@ -1,10 +1,12 @@
 """Learning routine"""
 
 import numpy as np
+import sys
+import os
 from tqdm import tqdm
 from sklearn.model_selection import RandomizedSearchCV
-from sklearn.utils._testing import ignore_warnings
-from sklearn.exceptions import ConvergenceWarning
+
+import warnings
 
 from config import (
     REGRESSORS,
@@ -15,26 +17,28 @@ from config import (
     N_ITER_SEARCH
 )
 
-@ignore_warnings(category=ConvergenceWarning)
 def train(train_data):
     """Learning method"""
-    hyperopts = np.empty((len(REGRESSORS), OUTPUT_SIZE), dtype=object)
-    for reg_idx in tqdm(range(len(REGRESSORS))):
-        for out_idx in range(OUTPUT_SIZE):
-            inp = train_data[:, :INPUT_SIZE]
-            target = train_data[:, INPUT_SIZE + out_idx]
-            rand_search = RandomizedSearchCV(
-                REGRESSORS[reg_idx][out_idx],
-                param_distributions=PARAM_DICTS[reg_idx],
-                n_iter=N_ITER_SEARCH,
-                cv=CV_FOLDS,
-                scoring='neg_root_mean_squared_error',
-                n_jobs=-1
-            )
-            rand_search.fit(
-                inp,
-                target
-            )
-            hyperopts[reg_idx, out_idx] = rand_search
+    if not sys.warnoptions:
+        warnings.simplefilter("ignore")
+        os.environ["PYTHONWARNINGS"] = "ignore" # Also affect subprocesses
+        hyperopts = np.empty((len(REGRESSORS), OUTPUT_SIZE), dtype=object)
+        for reg_idx in tqdm(range(len(REGRESSORS))):
+            for out_idx in range(OUTPUT_SIZE):
+                inp = train_data[:, :INPUT_SIZE]
+                target = train_data[:, INPUT_SIZE + out_idx]
+                rand_search = RandomizedSearchCV(
+                    REGRESSORS[reg_idx][out_idx],
+                    param_distributions=PARAM_DICTS[reg_idx],
+                    n_iter=N_ITER_SEARCH,
+                    cv=CV_FOLDS,
+                    scoring='neg_root_mean_squared_error',
+                    n_jobs=-1
+                )
+                rand_search.fit(
+                    inp,
+                    target
+                )
+                hyperopts[reg_idx, out_idx] = rand_search
 
     return hyperopts
