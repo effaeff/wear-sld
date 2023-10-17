5aada03477ca351b0dee0281bf8d0657e9fb8501
Felix Finkeldey <felix.finkeldey@tu-dortmund.de>
2023-10-17 10:49:51 +0200

    options for either learning stability or energy

--- scripts/infer.py
+++ scripts/infer.py
@@ -9,12 +9,16 @@ import itertools
 from wearsld.data_processing import DataProcessing
 from wearsld.utils import load_estimators
 
-from config import MODEL_DIR, PLOT_DIR, FONTSIZE
+from config import MODEL_DIR, PLOT_DIR, FONTSIZE, TARGET
+import misc
+from plot_utils import hist
 
 def infer_stability():
     """Inference method"""
     processing = DataProcessing()
     scaler = processing.get_scaler()
+
+    data = processing.get_data(TARGET)
 
     hyperopts = load_estimators(MODEL_DIR)
     for hyper_idx, hyperopt in enumerate(hyperopts):
@@ -40,6 +44,9 @@ def infer_stability():
 
             axs.plot(spsp, pred, label=f'{test_wear}')
 
+        sca = axs.scatter(data[:, 0], data[:, -1], c=data[:, 1], s=1.5)
+        plt.colorbar(sca)
+
         axs.legend(
             bbox_to_anchor=(0., 1.02, 1., .102),
             loc='lower left',
@@ -54,7 +61,7 @@ def infer_stability():
         axs.set_xlabel('Spindle speed', fontsize=FONTSIZE)
         axs.set_xticks(np.arange(4000, 8001, 1000))
         # axs.set_yticks(np.arange(0, 3.6, 0.5))
-        axs.set_yticks(np.arange(1, 6, 1))
+        axs.set_yticks(np.arange(0, 6, 1))
 
         fig.canvas.draw()
 
@@ -62,7 +69,7 @@ def infer_stability():
 
         axs.set_xlim(4000, 8000)
         # axs.set_ylim(0, 3.5)
-        axs.set_ylim(1, 5)
+        axs.set_ylim(0, 5)
         plt.tight_layout()
 
         # plt.show()
@@ -74,6 +81,7 @@ def infer_energy():
     processing = DataProcessing()
     scaler = processing.get_scaler()
     np.set_printoptions(suppress=True)
+    data = processing.get_data('limit')
 
     hyperopts = load_estimators(MODEL_DIR)
     for hyper_idx, hyperopt in enumerate(hyperopts):
@@ -93,14 +101,47 @@ def infer_energy():
             test_data_wear = scaler.transform(test_data_wear)
 
             pred = hyperopt[0].predict(test_data_wear)
+
+
+            pred = pred.reshape((len(spsp), len(ae)))
+            ae_limits = np.zeros_like(spsp)
+            for i_row, row in enumerate(pred):
+
+                limit = 10
+
+                # energy = np.zeros_like(row)
+                # for i in range(1, len(energy)):
+                    # slope = row[i]
+                    # energy[i] = 0
+                    # for j in range(i+1):
+                        # energy[i] += (row[j] - j * slope)**2
+                    # energy[i] /= i
+
+                try:
+                    # i_lim = np.where(energy > limit)[0][0]
+                    i_lim = np.where(row > limit)[0][0]
+                    ae_lim = ae[i_lim]
+                except:
+                    ae_lim = ae[-1]
+
+                ae_limits[i_row] = ae_lim
+
+                # __, energy_ax = plt.subplots(1, 1)
+                # energy_ax.plot(ae, row)
+                # energy_ax.axhline(np.std(row), 0, 1, c='r')
+                # plt.show()
+                # plt.close()
+
             axs.imshow(
-                pred.reshape((len(spsp), len(ae))).T,
+                pred.T,
                 cmap='inferno',
                 extent=[spsp[0], spsp[-1], ae[0], ae[-1]],
-                interpolation='antialiased',
+                # interpolation='antialiased',
                 origin='lower',
                 aspect='auto'
             )
+            sca = axs.scatter(data[:, 0], data[:, -1], c=data[:, 1], s=1.5)
+            # axs.plot(spsp, ae_limits)
 
             axs.set_ylabel(r'a$_e$', fontsize=FONTSIZE)
             axs.set_xlabel('Spindle speed', fontsize=FONTSIZE)
@@ -122,5 +163,7 @@ def infer_energy():
             plt.close()
 
 if __name__ == '__main__':
+    misc.gen_dirs([PLOT_DIR, MODEL_DIR])
+    # infer_stability()
     infer_energy()
 
